260eb33e1b647ed4c55262ae44fa290ec94c497a
Michael <mgwilliams112@gmail.com>
2026-06-17 21:53:03 -0400

    Added docstrings and other static analysis cleanup.

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+'''This module is the main app file. It imports the emotion detector function
+and provides an http route to access it from the frontend.'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,10 +7,13 @@ app = Flask('Emotion Detection')
 
 @app.route("/")
 def render_index_page():
+    '''Default route for displaying the home page.'''
     return render_template('index.html')
 
 @app.route('/emotionDetector')
 def emotion_detection():
+    '''Route to be called when text is submitted for analysis.
+Will return an error message if no text is entered.'''
     target_text = request.args.get('textToAnalyze')
     result = emotion_detector(target_text)
     anger = result['anger']
@@ -17,13 +22,12 @@ def emotion_detection():
     joy = result['joy']
     sadness = result['sadness']
     dominant = result['dominant_emotion']
-    if dominant == None:
-        return ('Invalid text! Please try again!')
-    return f'''For the given statement, the system response is 
-    'anger': {anger}, 'disgust': {disgust}, 
-    'fear': {fear}, 'joy': {joy}, and 'sadness': {sadness}. 
+    if dominant is None:
+        return 'Invalid text! Please try again!'
+    return f'''For the given statement, the system response is
+    'anger': {anger}, 'disgust': {disgust},
+    'fear': {fear}, 'joy': {joy}, and 'sadness': {sadness}.
     The dominant emotion is {dominant}.'''
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
